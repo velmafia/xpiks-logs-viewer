fe6fa73c34c925d72deeceb460a0d2ec7ec6bf0b
RostaTasha <natascha@rosta.ru>
2016-05-31 19:41:23 +0300

    added drug and drop

--- viewer.js
+++ viewer.js
@@ -20,28 +20,54 @@ function colorizer(text){
 	return colored_text;
 };
 
+function read_and_publish(file) {
+	var label	 = document.getElementById('inputlabel');
+	var textDisplayArea = document.getElementById('textDisplayArea');
+	
+	var reader = new FileReader();
+	
+	reader.onload = function(e) {
+		var text = reader.result;
+		textDisplayArea.innerHTML = colorizer(text);
+		textDisplayArea.style.visibility = "visible";
+		label.querySelector( 'span' ).innerHTML = file.name;
+	}
+	reader.readAsText(file);
+};
 
+
+ function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    var obj = evt.dataTransfer
+	var files;
+	if (obj == null){
+		var fileInput = evt.target;
+		files = fileInput.files;
+	} else {
+		files = obj.files; // FileList object.
+	}
+	f =files[0];
+	read_and_publish(f);
+  }
+
+function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+}
 
 window.onload = function() {
-		var fileInput = document.getElementById('files');
-		var label	 = fileInput.nextElementSibling;
-		var textDisplayArea = document.getElementById('textDisplayArea');
+	var fileInput = document.getElementById('files');
+	var dropZone = document.getElementById('box');
 
-		fileInput.addEventListener('change', function(e) {
-			var file = fileInput.files[0];
-			var fileName = e.target.value.split( '\\' ).pop();
-			if (fileName){
-				label.querySelector( 'span' ).innerHTML = fileName;
-
-				var reader = new FileReader();
-
-				reader.onload = function(e) {
-					var text = reader.result;
-					textDisplayArea.innerHTML = colorizer(text);
-					textDisplayArea.style.visibility = "visible";
-				}
-
-				reader.readAsText(file);
-			}				
-		});
+	fileInput.addEventListener('change', handleFileSelect);
+	dropZone.addEventListener('dragover', handleDragOver, false);
+	dropZone.addEventListener('drop', handleFileSelect, false);
 }
+
+
+
+
+
+  
